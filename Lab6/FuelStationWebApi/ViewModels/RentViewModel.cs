fc02ffdc21bf839e29c640d29d6a3774a5cc9696
irenshinkorenko <ishinkorenko98@gmail.com>
2018-05-25 17:38:45 +0300

    fixed lab6

---
+++ Lab6/FuelStationWebApi/ViewModels/RentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application.ViewModels
+{
+    public class DoctorViewModel
+    {        
+        public int DoctorID { get; set; }
+            
+        
+        public string DoctorName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+        public string PatientName { get; set; }
+    }
+}
